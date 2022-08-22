fa9f40a6ba4a01af7a7225d29f4282ef31fd405b
Gulyutin <gulyutin@gmail.com>
2022-08-22 20:36:23 +0700

    EX54 updated

---
+++ EX54/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine());
+int [,] array = new int[m, n];
+
+
+Random rand = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int  j = 0; j < n; j++)
+    {
+        array[i, j] = rand.Next(1, 100);
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine("");
+}
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+}
+
+Console.WriteLine($"\nОтсортированный массив: ");
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+
